67a9a4e2884e3f93075d72d63ade152110293f91
B0neh3ad <js1044k@gmail.com>
2024-08-05 02:51:10 +0900

    Add LB

---
+++ src/app/api/sheet-data/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const API_KEY = process.env.GAPI_KEY;
+const SPREADSHEET_ID = process.env.SPREADSHEETS_ID;
+const RANGE = '리더보드!A4:I51';
+
+export interface TeamInfo {
+    name: string;
+    ssimScore: number;
+    rank: number;
+};
+
+function parseData(data: any): TeamInfo[] {
+    const parsedData: TeamInfo[] = [];
+    data.values.forEach((row: any) => {
+        for(let i = 0; i < row.length; i += 3) {
+            const newRow: TeamInfo = {
+                name: row[i] || '',
+                ssimScore: Number(row[i + 1]),
+                rank: Number(row[i + 2]),
+            };
+            parsedData.push(newRow);
+        }
+    })
+    return parsedData;
+}
+
+function compareData(a: TeamInfo, b: TeamInfo) {
+    return b.ssimScore - a.ssimScore === 0 ? 
+    a.name.localeCompare(b.name) : b.ssimScore - a.ssimScore;
+}
+
+export async function GET(req: NextRequest) {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const sortedData: TeamInfo[] = parseData(data).sort(compareData);
+
+    return NextResponse.json(sortedData);
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,65 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useEffect, useState } from "react";
+import './page.css';
+import { TeamInfo } from "./api/sheet-data/route";
+import { useRouter } from "next/router";
+
+function formatDateTime(now: Date) {
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  return `${year}년 ${month}월 ${date}일 ${hours}:${minutes}`
+}
 
 export default function Home() {
+  const [data, setData] = useState<TeamInfo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const now = new Date();
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('/api/sheet-data')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <header>
+        <h1>2024 FastMRI LeaderBoard</h1>
+        <p>Last Update:<br />{formatDateTime(now)}</p>
+      </header>
+      <div id="leaderboard">
+        {loading ?
+          <div style={{'textAlign': 'center'}}>로딩 중...</div> :
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Team Name</th>
+                <th>SSIM Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((teamInfo, idx) => (
+                <tr key={teamInfo.name} className={teamInfo.rank <= 5 ? "top" : ""}>
+                  <td className="rank">{teamInfo.rank}</td>
+                  <td className="name">{teamInfo.name}</td>
+                  <td className="score">{teamInfo.ssimScore}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          }
       </div>
     </main>
   );

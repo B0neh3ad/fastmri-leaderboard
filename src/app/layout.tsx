af09d3be406a0fff78bde8401336be13ec6305a3
B0neh3ad <js1044k@gmail.com>
2024-08-05 04:02:54 +0900

    Change font and update css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -10,7 +8,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css' />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
